d35a6a66ba24a2626a0e798f00fc8d08d7ceb1aa
Yochiro lee <yleecruz@gmail.com>
2025-07-28 17:15:46 -0400

    ajustes

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -2,7 +2,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
-  const data = await req.json();
+  console.log('see data', await req.json());
 
   // Simula respuesta
   return NextResponse.json({ success: true, message: 'Correo enviado' });
