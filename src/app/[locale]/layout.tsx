8df461e07ba86cc61cafd229f8fbb16f06e9dd9b
Yochiro lee <yleecruz@gmail.com>
2025-07-28 12:24:20 -0400

    cart drawer scroll

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,11 +1,6 @@
 import { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
-import { CartProvider } from "../../context/CartContext"; 
-import { Toaster } from "sonner";
 
-const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
-const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
 export async function generateMetadata(props: {
   params: Promise<{ locale: string }>;
@@ -97,8 +92,7 @@ export async function generateMetadata(props: {
 }
 
 export default async function LocaleLayout({
-  children,
-  params,
+  children,  
 }: {
   children: React.ReactNode
   params: Promise<{ locale: string }>
